453c49bd84225f926beedc6c6addd190b5e82836
dabear <bjorninge@gmail.com>
2021-10-07 23:36:46 +0200

    bluebuttonfix ios15

--- LibreTransmitterUI/Views/Styles/BlueButtonStyle.swift
+++ LibreTransmitterUI/Views/Styles/BlueButtonStyle.swift
@@ -11,12 +11,16 @@ import SwiftUI
 struct BlueButtonStyle: ButtonStyle {
 
   func makeBody(configuration: Self.Configuration) -> some View {
-    configuration.label
-        .font(.headline)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        .contentShape(Rectangle())
-        .padding()
-        .foregroundColor(configuration.isPressed ? Color.white.opacity(0.5) : Color.white)
-        .listRowBackground(configuration.isPressed ? Color.blue.opacity(0.5) : Color.blue)
+
+      configuration.label
+          .font(.headline)
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+          .contentShape(Rectangle())
+          .padding()
+          .foregroundColor(configuration.isPressed ? Color.white.opacity(0.5) : Color.white)
+          .background(configuration.isPressed ? Color.blue.opacity(0.5) : Color.blue)
+          .cornerRadius(10)
+
+
   }
 }
