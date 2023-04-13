b1832b9f705fb390f25a57f44d649add392b9c1d
dabear <dabear@users.noreply.github.com>
2023-04-13 11:34:23 +0200

    Features.swift update, add supportsFakeSensor

--- Features.swift
+++ Features.swift
@@ -2,8 +2,8 @@
 //  Features.swift
 //  LibreTransmitter
 //
-//  Created by Bjørn Inge Berg on 30/08/2021.
-//  Copyright © 2021 Mark Wilson. All rights reserved.
+//  Created on 30/08/2021.
+//  Copyright © 2021 LoopKit Authors. All rights reserved.
 //
 
 import Foundation
@@ -14,12 +14,16 @@ import CoreNFC
 
 public final class Features {
 
-    static public var logSubsystem = "no.bjorninge.libre"
+    static public var logSubsystem = "com.loopkit.libre"
     
     static public var glucoseSettingsRequireAuthentication = false
     static public var alarmSettingsViewRequiresAuthentication = false
     
     static public var allowsEditingFactoryCalibrationData = false
+    
+    // Only to be used with this program, running on a linux amd64 system or rasberry pi
+    // https://github.com/tzachi-dar/gatt#this-is-a-program-for-creating-a-simulation-for-libre-2-for-xdrip
+    static public var supportsFakeSensor = false
 
     static var phoneNFCAvailable: Bool {
         #if canImport(CoreNFC)
